188edc420806e15912c880195c2b71bfbc87703b
Manuel Schmidbauer <manuel2258@protonmail.com>
2019-12-08 16:24:50 +0100

    ConfigParser implemented

--- app/src/main/java/hm/edu/launcher/config/parser/IConfigurationParser.java
+++ app/src/main/java/hm/edu/launcher/config/parser/IConfigurationParser.java
@@ -1,9 +1,12 @@
 package hm.edu.launcher.config.parser;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import hm.edu.launcher.config.container.ConfigurationContainer;
 
 public interface IConfigurationParser {
 
-    ConfigurationContainer parseConfig(String path);
+    ConfigurationContainer parseConfig(InputStream fileStream) throws IOException, ConfigParseException;
 
 }
--- app/src/main/java/hm/edu/launcher/config/parser/XmlParserV1.java
+++ app/src/main/java/hm/edu/launcher/config/parser/XmlParserV1.java
@@ -1,11 +1,62 @@
 package hm.edu.launcher.config.parser;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import hm.edu.launcher.config.container.AppContainer;
 import hm.edu.launcher.config.container.ConfigurationContainer;
 
 public class XmlParserV1 implements IConfigurationParser {
 
     @Override
-    public ConfigurationContainer parseConfig(String path) {
-        return null;
+    public ConfigurationContainer parseConfig(InputStream xmlStream) throws IOException, ConfigParseException {
+        ConfigurationContainer container = new ConfigurationContainer();
+
+        try {
+            DocumentBuilder xmlBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+
+            Document xmlDocument = xmlBuilder.parse(xmlStream);
+
+            NodeList appList = xmlDocument.getElementsByTagName("app");
+
+            AppContainer[] apps = new AppContainer[appList.getLength()];
+
+            for (int i = 0; i < appList.getLength(); i++) {
+                if (appList.item(i).getNodeType() == Node.ELEMENT_NODE) {
+                    final Element appElement = (Element) appList.item(i);
+                    final int appId = Integer.parseInt(appElement.getAttribute("id"));
+                    final String appName = appElement.getTextContent();
+                    if(apps.length <= appId) {
+                        throw new ConfigParseException(String.format("Id %d is bigger then length", appId));
+                    }
+                    if(apps[appId] != null) {
+                        throw new ConfigParseException(String.format("Id %d is contained multiple times", appId));
+                    }
+                    apps[appId] = new AppContainer(appName);
+                }
+            }
+            for (int i = 0; i < apps.length; i++) {
+                AppContainer app = apps[i];
+                if(app == null) {
+                    throw new ConfigParseException(String.format("Id %d is not contained", i));
+                }
+                container.add(app);
+            }
+
+        } catch (ParserConfigurationException | SAXException e) {
+            throw new IOException(e);
+        }
+
+        return container;
     }
 }
