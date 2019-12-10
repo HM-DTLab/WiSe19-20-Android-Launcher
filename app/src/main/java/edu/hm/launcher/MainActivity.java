e0eaaa2c23d35ee7e4eacb47b2473d98af0e43b0
hm-mani <mani_hamza@hotmail.com>
2019-12-10 19:37:52 +0100

    new Activity

--- app/src/main/java/edu/hm/launcher/MainActivity.java
+++ app/src/main/java/edu/hm/launcher/MainActivity.java
@@ -1,5 +1,11 @@
 package edu.hm.launcher;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -9,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public static Drawable getActivityIcon(Context context, String packageName, String activityName) {
+        PackageManager pm = context.getPackageManager();
+        Intent intent = new Intent();
+        intent.setComponent(new ComponentName(packageName, activityName));
+        ResolveInfo resolveInfo = pm.resolveActivity(intent, 0);
+
+        return resolveInfo.loadIcon(pm);
     }
 }
