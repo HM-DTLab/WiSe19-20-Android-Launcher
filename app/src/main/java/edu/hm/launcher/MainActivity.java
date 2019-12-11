513d0ccae81c0ac9f4b1b7d8184d31fa09600ac7
hm-mani <mani_hamza@hotmail.com>
2019-12-11 13:14:04 +0100

    _final_ version

--- app/src/main/java/edu/hm/launcher/MainActivity.java
+++ app/src/main/java/edu/hm/launcher/MainActivity.java
@@ -8,6 +8,7 @@ import android.content.pm.ResolveInfo;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,12 +18,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public static Drawable getActivityIcon(Context context, String packageName, String activityName) {
-        PackageManager pm = context.getPackageManager();
-        Intent intent = new Intent();
-        intent.setComponent(new ComponentName(packageName, activityName));
-        ResolveInfo resolveInfo = pm.resolveActivity(intent, 0);
-
-        return resolveInfo.loadIcon(pm);
+    /**
+     * opens a new window where all the apps are listed when the plus button is clicked
+     * @param view
+     */
+    public void onPlusBottonClick(View view){
+        Intent intent = new Intent(this, AppsDrawer.class);
+        startActivity(intent);
     }
+
+    /**
+     * opens the chrome app when the chrome image is clicked
+     * @param v
+     */
+    public void onChromeButtonClick(View v) {
+        Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.android.chrome");
+        startActivity(launchIntent);
+    }
+
+
+
+
+//    this function gives the icon of an app back
+//    public static Drawable getActivityIcon(Context context, String packageName, String activityName) {
+//        PackageManager pm = context.getPackageManager();
+//        Intent intent = new Intent();
+//        intent.setComponent(new ComponentName(packageName, activityName));
+//        ResolveInfo resolveInfo = pm.resolveActivity(intent, 0);
+//
+//        return resolveInfo.loadIcon(pm);
+//    }
 }
